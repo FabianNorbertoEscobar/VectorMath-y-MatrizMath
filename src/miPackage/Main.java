6357eb9d0353a1b20ce8f57095766881b0472643
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-09-13 21:40:51 -0300

    Se agrega la potencia de una matriz y un nuevo constructor

--- src/miPackage/Main.java
+++ src/miPackage/Main.java
@@ -72,23 +72,23 @@ public class Main {
 		mb.mostrar();
 
 		MatrizMath mc = new MatrizMath(2, 2);
-		
+
 		System.out.println("Matriz A + Matriz B:");
 		mc = ma.sumar(mb);
 		mc.mostrar();
-		
+
 		System.out.println("Matriz A - Matriz B:");
 		mc = ma.restar(mb);
 		mc.mostrar();
-		
+
 		System.out.println("Matriz A * Matriz B:");
 		mc = ma.multiplicar(mb);
 		mc.mostrar();
-		
+
 		System.out.println("Matriz A * 2:");
 		mc = ma.multiplicar(2);
 		mc.mostrar();
-		
+
 		System.out.println("Norma1 de Matriz A:");
 		d = ma.norma1();
 		System.out.println(d);
@@ -107,28 +107,37 @@ public class Main {
 		if (ma.equals(mc)) {
 			System.out.println("Matriz A es igual a Matriz C");
 		}
-		
+
 		System.out.println("Matriz identidad orden 3");
 		MatrizMath.identidad(3).mostrar();
-		
+
 		System.out.println("Determinante de la Matriz A:");
 		d = ma.determinante();
 		System.out.println(d);
-		
+
 		System.out.println("Matriz inversa de A:");
 		mc = ma.inversa();
 		mc.mostrar();
-		
+
 		System.out.println("Error al invertir:");
 		d = ma.errorAlInvertir();
 		System.out.println(d);
-		
+
 		System.out.println("Vector A * Matriz Identidad:");
 		c = a.multiplicar(MatrizMath.identidad(6));
 		c.mostrar();
-		
+
 		System.out.println("Matriz Identidad * Vector A");
 		c = MatrizMath.identidad(6).multiplicar(a);
 		c.mostrar();
+
+		double[][] mat = { { 3, 1, 2 }, { 3, 9, 5 }, { 1, 7, 6 } };
+		MatrizMath matriz = new MatrizMath(3, 3, mat);
+		MatrizMath potencia = matriz.potencia(5);
+		potencia.mostrar();
+
+		// 27651.0 83765.0 58026.0
+		// 111583.0 337665.0 233801.0
+		// 98869.0 299379.0 207202.0
 	}
 }
--- src/miPackage/MatrizMath.java
+++ src/miPackage/MatrizMath.java
@@ -27,10 +27,15 @@ public class MatrizMath {
 		this.valor[fila][columna] = valor;
 	}
 
-	public MatrizMath(int fila, int columna) {
-		this.filas = fila;
-		this.columnas = columna;
-		valor = new double[fila][columna];
+	public MatrizMath(int filas, int columnas) {
+		this.filas = filas;
+		this.columnas = columnas;
+		valor = new double[filas][columnas];
+	}
+
+	public MatrizMath(int filas, int columnas, double[][] matriz) {
+		this(filas, columnas);
+		this.valor = matriz.clone();
 	}
 
 	public MatrizMath(String path) {
@@ -55,7 +60,7 @@ public class MatrizMath {
 			System.out.println("No se pudo abrir el archivo");
 		}
 	}
-	
+
 	private MatrizMath sumar(MatrizMath matriz, int signo) throws MatrizMathException {
 
 		if (this.filas != matriz.filas || this.columnas != matriz.columnas) {
@@ -70,7 +75,7 @@ public class MatrizMath {
 		}
 		return suma;
 	}
-	
+
 	public MatrizMath sumar(MatrizMath matriz) throws MatrizMathException {
 		return this.sumar(matriz, 1);
 	}
@@ -280,5 +285,26 @@ public class MatrizMath {
 			}
 			System.out.println();
 		}
+	}
+
+	public MatrizMath potencia(int exponente) {
+		MatrizMath base = this.clone();
+		if (!this.cuadrada()) {
+			throw new MatrizMathException("La potencia solo se aplica a matrices cuadradas");
+		} else if (exponente == 0) {
+			return MatrizMath.identidad(this.filas);
+		} else if (exponente < 0) {
+			exponente *= -1;
+			for (int i = 0; i < base.getFilas(); i++) {
+				for (int j = 0; j < base.getColumnas(); j++) {
+					base.setValor(i, j, Math.pow(base.getValor(i, j), -1));
+				}
+			}
+		}
+		MatrizMath potencia = base.clone();
+		for (int i = 1; i < exponente; i++) {
+			potencia = potencia.multiplicar(base);
+		}
+		return potencia;
 	}
 }
